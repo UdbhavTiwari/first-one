ca2e05bea2b6eed68f0ce616210d33f066249d71
Udbhav Tiwari <rj07p5483secondid@gmail.com>
2024-05-23 00:26:30 +0530

    adding Diff.java

---
+++ Diff.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Diff {
+        
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+
+        System.out.println("difference is " +(a-b));
+    }
+}
